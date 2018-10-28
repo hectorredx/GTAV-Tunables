e203890e1229a11b7fb3c2dce3cb078e9bf39192
MikeRohsoft <26527907+MikeRohsoft@users.noreply.github.com>
2018-10-28 21:18:46 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,47 +1,24 @@
-var http        = require('http-request');
-var fs          = require('fs');
-var util        = require('./util');
-var aesjs       = require('aes-js');
-var beautify    = require('js-beautify').js_beautify;
-var key         = "F06F12F49B843DADE4A7BE053505B19C9E415C95D93753450A269144D59A0115";
-
-console.log('Fetching latest tunables...');
-
-http.get({
-    url: 'http://prod.cloud.rockstargames.com/titles/gta5/pcros/0x1a098062.json',
-    progress: function(current,total) {
-        console.log('%d% fetched...', Math.round(current / total * 100));
-    }
-}, function(err, res){
-    if(err) {
-        console.log('Couldn\'t fetch tunables - strange...');
-		return;
-    }
-    console.log('Done fetching!');
-    console.log('Decrypting tunables...');
-
-    var decryptedTunables = decryptTunables(res.buffer);
-
-    if(decryptedTunables.substring(0,5) == 'error') {
-        console.log('Couldn\'t decrypt tunables - error: ' + decryptedTunables.substring(6));
-        return;
-    }
-	
-	//fs.unlink('tunables.json');
-	fs.writeFileSync('tunables.json', beautify(decryptedTunables));
-	console.log('Done!');
-});
+const fs = require('fs');
+const http = require('./lwp');
+const beautify = require('js-beautify').js_beautify;
+const aesjs = require('aes-js');
+const key = Buffer.from('F06F12F49B843DADE4A7BE053505B19C9E415C95D93753450A269144D59A0115', 'hex');
+const url = 'http://prod.cloud.rockstargames.com/titles/gta5/pcros/0x1a098062.json';
 
 function decryptTunables(encrypted) {
-    var encryptedLength = Math.floor(encrypted.length / 16);
+    const encryptedLength = encrypted.length - (encrypted.length % 16);
+    const aesEcb = new aesjs.ModeOfOperation.ecb(key);
+    const decryptedBytes = aesEcb.decrypt(encrypted.slice(0, encryptedLength));
+    return Buffer.from(decryptedBytes).toString() + encrypted.slice(encryptedLength, encrypted.length).toString();
+};
 
-    var nipples = new Buffer(encrypted.toString('hex').substring(encryptedLength * 16 * 2), 'hex').toString('utf8'); //Data that is not encrypted (because rockstar doesn't know how to ^16bit)
-    var body = new Buffer(encryptedLength * 16);
-    for(var c = 0; c < encryptedLength * 16; c++) {
-        body[c] = encrypted[c];
-    }
-
-    var aesEcb = new aesjs.ModeOfOperation.ecb(new Buffer(key, 'hex'));
-    var decryptedBytes = aesEcb.decrypt(body);
-    return aesjs.util.convertBytesToString(decryptedBytes) + nipples;
-}
+console.log('Download Tunables...');
+http.get(url).then(res => {
+	const filename = `tunables-${new Date().getTime()}.json`;
+	console.log('Done!\nDecrypting Files...');
+	let decrypted = decryptTunables(res.content);
+	console.log('Done!\nBeautify it...');
+	const beautified = beautify(decrypted);
+	console.log('Done!\nWrite to File ...');
+	fs.writeFile(filename, beautified, null, () => console.log(`Done!\n${filename}`));
+});
