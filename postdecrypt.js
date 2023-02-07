feac7eaf58f97083d73b88452a6ab0f84ab66060
hectorredx <hectorredx@gmail.com>
2023-02-07 07:55:34 +0100

    Refactor pre/post decryption

--- postdecrypt.js
+++ postdecrypt.js
@@ -2,18 +2,25 @@ const fs = require('fs');
 const beautify = require('js-beautify').js_beautify;
 const _findKey = require('lodash').findKey;
 const _set = require('lodash').set;
-const _uniqWith = require('lodash').unionWith;
-const _isEqual = require('lodash').isEqual;
+const joaat = require('./lib/joaat');
 
+const FILE_NAME_IN = `tunables-encrypted.json`;
+const FILE_NAME_OUT = `tunables-decrypted.json`;
+const FILE_NAME_DICTIONARY = `dictionary.json`;
+const FILE_NAME_NOT_FOUND = `tunables-not-found.json`;
+
+const CONTENT_MODIFIER_CONTEXT_BASE = 'CONTENT_MODIFIER_';
 
 const TUNABLE_CONTEXT = {
     BASE_GLOBALS: 190769267,
     MP_Global: 953070135,
+    CD_GLOBAL: 3819947453,
     MP_CNC: 1398379561,
     MP_CNC_TEAM_COP: 3999338632,
     MP_CNC_TEAM_VAGOS: 3137655631,
     MP_CNC_TEAM_LOST: 988790432,
     MP_FM: 1140746429,
+    MP_FM_MEMBERSHIP: 3749990885,
     MP_FM_DM: 1882254284,
     MP_FM_RACES: 3273869472,
     MP_FM_RACES_CAR: 1031194139,
@@ -34,56 +41,51 @@ const TUNABLE_CONTEXT = {
     MP_FM_GANG_ATTACK: 1744317449,
     MP_FMADVERSARY: 2615070496,
 };
-const TUNABLE_CONTEXT_VALUES = Object.values(TUNABLE_CONTEXT);
+const TUNABLE_DYNAMIC_CONTEXT_CONTENT_MODIFIER = {};
 
-const TUNEABLES_PROCESSING_FILE_PATH = "tuneables_processing.c";
-const tuneablesProcessingRawData = fs.readFileSync(TUNEABLES_PROCESSING_FILE_PATH);
-
-const dictionaryFileRawData = fs.readFileSync('dictionary.json');
+const dictionaryFileRawData = fs.readFileSync(FILE_NAME_DICTIONARY);
 const dictionary = JSON.parse(dictionaryFileRawData);
 let tunablesData = {};
 let tunablesDataDecrypted = {};
 
-const filenameIn = `tunables-encrypted.json`;
-const filenameOut = `tunables-decrypted.json`;
-
-const tunablesFileRawData = fs.readFileSync(filenameIn);
+const tunablesFileRawData = fs.readFileSync(FILE_NAME_IN);
 tunablesData = JSON.parse(tunablesFileRawData);
 tunablesDataDecrypted = { ...tunablesData, tunables: {} };
+tunablesData.contentlists[0].forEach((_, i) => {
+    TUNABLE_DYNAMIC_CONTEXT_CONTENT_MODIFIER[CONTENT_MODIFIER_CONTEXT_BASE.concat(i)] = joaat(CONTENT_MODIFIER_CONTEXT_BASE.concat(i))
+});
+let totalDecryptedTunables = 0;
 let notFound = [];
 
 console.log('Total encrypted tunables = ', Object.keys(tunablesData.tunables).length);
 Object.keys(tunablesData.tunables).forEach(async key => {
-    let checkIndex = 0;
-    let fallbacks = [];
-    while (checkIndex < TUNABLE_CONTEXT_VALUES.length) {
-        const hash = parseInt(key, 16) - TUNABLE_CONTEXT_VALUES[checkIndex];
-        const result = _findKey(dictionary, (x) => x.toLowerCase() === hash.toString(16));
-        if (result) {
-            _set(tunablesDataDecrypted.tunables, _findKey(TUNABLE_CONTEXT, (x) => x === TUNABLE_CONTEXT_VALUES[checkIndex]) + '.' + result, tunablesData.tunables[key])
-            checkIndex = 0;
+    let found = false;
+    for (const [contextKey, contextValue] of Object.entries(TUNABLE_CONTEXT)) {
+        const hash = parseInt(key, 16) - contextValue;
+        const tunableName = _findKey(dictionary, (x) => [x.signed, x.unsigned].includes(hash));
+        if (tunableName) {
+            _set(tunablesDataDecrypted.tunables, `${contextKey}.${tunableName}`, tunablesData.tunables[key]);
+            found = true;
+            totalDecryptedTunables++;
             break;
-        } else if (tuneablesProcessingRawData.includes(hash)) {
-            fallbacks.push(hash);
-
-            if (checkIndex === TUNABLE_CONTEXT_VALUES.length - 1) {
-                const fallback = fallbacks.shift();
-                _set(tunablesDataDecrypted.tunables, _findKey(TUNABLE_CONTEXT, (x) => x === TUNABLE_CONTEXT_VALUES[0]) + '.' + fallback, tunablesData.tunables[key])
-                fallbacks = [];
-                break;
-            }
-        } else {
-            if (!Object.keys(notFound).includes(key)) notFound.push({
-                [key]: tunablesData.tunables[key]
-            })
         }
-
-        checkIndex++;
     }
+    for (const [contextDynamicKey, contextDynamicValue] of Object.entries(TUNABLE_DYNAMIC_CONTEXT_CONTENT_MODIFIER)) {
+        const hashSigned = parseInt(key, 16) - contextDynamicValue.signed;
+        const hashUnsigned = parseInt(key, 16) - contextDynamicValue.unsigned;
+        const tunableName = _findKey(dictionary, (x) => x.signed === hashSigned || x.unsigned === hashUnsigned);
+        if (tunableName) {
+            _set(tunablesDataDecrypted.tunables, `${contextDynamicKey}.${tunableName}`, tunablesData.tunables[key]);
+            found = true;
+            totalDecryptedTunables++;
+            break;
+        }
+    }
+    if (!found && !Object.keys(notFound).includes(key)) notFound.push({
+        [key]: tunablesData.tunables[key]
+    });
 });
-console.log('Total decrypted tunables = ', Object.keys(tunablesDataDecrypted.tunables.BASE_GLOBALS).length + Object.keys(tunablesDataDecrypted.tunables.MP_Global).length)
-notFound = _uniqWith(notFound, _isEqual);
+console.log('Total decrypted tunables = ', totalDecryptedTunables);
 console.log('Total not found tunables = ', notFound.length);
-fs.writeFile(filenameOut, beautify(JSON.stringify(tunablesDataDecrypted)), null, () => console.log('Done!'));
-fs.writeFile('tunables-not-found.json', beautify(JSON.stringify(notFound)), null, () => console.log(''));
-// fs.unlinkSync(filenameIn)
+fs.writeFile(FILE_NAME_OUT, beautify(JSON.stringify(tunablesDataDecrypted)), null, () => console.log('Done!'));
+fs.writeFile(FILE_NAME_NOT_FOUND, beautify(JSON.stringify(notFound)), null, () => console.log(''));
--- predecrypt.js
+++ predecrypt.js
@@ -4,19 +4,13 @@ const beautify = require('js-beautify').js_beautify;
 const joaat = require('./lib/joaat');
 
 const tunableNamesUrl = 'https://raw.githubusercontent.com/Wildbrick142/V-Tunable-Names/main/tunable_list.txt';
-const tuneablesProcessingUrl = 'https://raw.githubusercontent.com/Aure7138/GTAV-Decompiled-Scripts/94da660931956e4d57cdc735479973b135d84723/decompiled_scripts/tuneables_processing.c';
 
 const dictionary = {};
 
 http.get(tunableNamesUrl).then((response) => {
     response.content.toString().split(/\r?\n/).forEach(line => {
-        if (line.length) dictionary[line] = joaat(line).hex
+        if (line.length) dictionary[line] = joaat(line);
     });
     if (Object.keys(dictionary).length) fs.writeFileSync('dictionary.json', beautify(JSON.stringify(dictionary)));
-    console.log('Tunables Dictionary downloaded')
-})
-
-http.get(tuneablesProcessingUrl).then((response) => {
-    fs.writeFileSync('tuneables_processing.c', response.content.toString());
-    console.log('Decompiled tuneables_processing.c downloaded')
-})
+    console.log('Tunables Dictionary downloaded');
+});
